7da93d5f3a1a23490848a9d2e26e7133fefd2441
Tiago Hoinack <tiagohoinack@hotmail.com>
2025-09-07 17:31:21 -0300

    feat(server): add basic endpoints and start server on configurable port

--- api/server.js
+++ api/server.js
@@ -1,11 +1,31 @@
 import express from "express";
-
 const app = express();
+const port = 3000;
 
-app.listen(3000, () => {
-  console.log("Servidor iniciado na porta 3000");
+app.listen(port, () => {
+  console.log(`Servidor iniciado na porta ${port}`);
+});
+
+app.get("/api/perguntas", async (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Endpoint /API/PERGUNTAS acessado com sucesso" });
 });
 
 app.post("/api/usuario", async (req, res) => {
-console.log("Consegui bater via POST na API Usuário");
+  res
+    .status(200)
+    .json({ message: "Endpoint /API/USUARIOS acessado com sucesso" });
+});
+
+app.post("/api/questionario/email", async (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Endpoint /API/QUESTIONARIO/EMAIL acessado com sucesso" });
+});
+
+app.put("/api/questionario", async (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Endpoint /API/QUESTIONARIO acessado com sucesso" });
 });
